96bdd097546f57b68295a92a0687c3c8b68714df
kozfogel <github@3lmo.de>
2017-11-18 13:56:33 +0100

    init

---
+++ affi/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace affi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Total # of processors: {0}", Environment.ProcessorCount);
+
+            var readTxt = File.ReadAllLines("affi.txt");
+
+            List<Affinfo> affis = new List<Affinfo>();
+
+            foreach(var l in readTxt)
+            {
+                affis.Add(new Affinfo(l));
+            }
+
+
+            while(true)
+            {
+                Console.WriteLine("Scanning ...");
+                foreach(var p in Process.GetProcesses())
+                {
+                    foreach (Affinfo ai in affis)
+                        if(p.ProcessName.ToLower() == ai.Name.ToLower())
+                            if(p.ProcessorAffinity != (System.IntPtr)ai.TargetAffinitiy)
+                            {
+                                Console.WriteLine("found: " + ai.Name + " set to " + ai.TargetAffinitiy);
+                                p.ProcessorAffinity = (System.IntPtr)ai.TargetAffinitiy;
+                            }
+                }
+
+                Thread.Sleep(5000);
+            }
+            
+        }
+    }
+}
---
+++ affi/affinfo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace affi
+{
+    class Affinfo
+    {
+        public int TargetAffinitiy;
+        public String Name;
+
+        public Affinfo(string line)
+        {
+            string name = line.Split(':')[0];
+            string affi = line.Split(':')[1];
+            
+            while (affi.Length < Environment.ProcessorCount)
+                affi += "0";
+
+            string raffi = Reverse(affi);
+
+            int newAffinity = Convert.ToInt32(raffi, 2);
+            this.TargetAffinitiy = newAffinity;
+            this.Name = name;
+
+            Console.WriteLine("Create target: " + this.Name + " (" + this.TargetAffinitiy + " = " + affi + ")");
+        }
+
+        public static string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+    }
+}
